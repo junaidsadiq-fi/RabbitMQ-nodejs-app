b8341ec7366978cf4e662f46a39070492f59089a
Junaid <junaid.sadiq009@gmail.com>
2024-05-05 08:36:55 +0300

    removed minor errors

--- backend/server-b/src/app.js
+++ backend/server-b/src/app.js
@@ -1,6 +1,6 @@
 const { getTask } = require('../rabbit-utils/receiveTask.js');
 
-const rabbitHost = process.env.RABBIT_HOST;
+const rabbitHost = process.env.RABBIT_HOST || 'localhost';
 const queueName = 'order_queue'; // The queue name to receive orders from Server A
 
 // receiving tasks from the queue a
